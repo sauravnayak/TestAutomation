ede4c7ecabdbe4c548493d01bdd27826dbc61dd6
sauravnayak <sauravnayak999@gmail.com>
2019-07-18 02:22:43 +0530

    ggn

--- com.testautomation.selenium/src/main/java/com/testautomation/selenium/capability/DesiredCapability.java
+++ com.testautomation.selenium/src/main/java/com/testautomation/selenium/capability/DesiredCapability.java
@@ -12,7 +12,7 @@ public class DesiredCapability {
 	public static WebDriver wd;
 	
 	public WebDriver getDriver(){
-		//Adding Chrdome Capabili tcies tdo Driverc s
+		//Adding Chrdomde Capabili tcies tdo Driverc s
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--disable-notifications");
 		options.setPageLoadStrategy(PageLoadStrategy.NONE);
